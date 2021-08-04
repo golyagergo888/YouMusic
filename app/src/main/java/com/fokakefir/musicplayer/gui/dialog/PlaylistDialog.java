0b9beed3102c187fc9a541117f5ad53cf0d87c1a
FokaKefir <babosdavid8@gmail.com>
2021-08-04 17:30:51 +0300

    Adding new design to the playlist dialog.

--- app/src/main/java/com/fokakefir/musicplayer/gui/dialog/PlaylistDialog.java
+++ app/src/main/java/com/fokakefir/musicplayer/gui/dialog/PlaylistDialog.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
@@ -17,11 +18,14 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
 import com.fokakefir.musicplayer.R;
+import com.fokakefir.musicplayer.logic.database.MusicPlayerContract;
+import com.fokakefir.musicplayer.model.Playlist;
 
 public class PlaylistDialog extends AppCompatDialogFragment implements AdapterView.OnItemSelectedListener {
 
     private EditText txtPlaylist;
     private Spinner spinner;
+    private ImageView imgPlaylist;
 
     private String strName;
     private String strColor;
@@ -75,16 +79,20 @@ public class PlaylistDialog extends AppCompatDialogFragment implements AdapterVi
 
         this.txtPlaylist = view.findViewById(R.id.txt_new_playlist);
         this.spinner = view.findViewById(R.id.spinner_colors);
+        this.imgPlaylist = view.findViewById(R.id.img_new_playlist);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.colors, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         this.spinner.setAdapter(adapter);
         this.spinner.setOnItemSelectedListener(this);
 
+        setImgPlaylistColor(Playlist.COLOR_RED);
+
         if (this.strName != null && this.strColor != null) {
             this.txtPlaylist.setText(strName);
             int position = adapter.getPosition(this.strColor);
             this.spinner.setSelection(position);
+            setImgPlaylistColor(this.strColor);
         }
 
         return builder.create();
@@ -94,11 +102,34 @@ public class PlaylistDialog extends AppCompatDialogFragment implements AdapterVi
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
         this.strColor = parent.getItemAtPosition(position).toString();
+        setImgPlaylistColor(this.strColor);
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+    }
+
+    public void setImgPlaylistColor(String strColor) {
+        int color = R.color.playlistWhite;
+        switch (strColor) {
+            case Playlist.COLOR_RED:
+                color = R.color.playlistRed;
+                break;
+            case Playlist.COLOR_ORANGE:
+                color = R.color.playlistOrange;
+                break;
+            case Playlist.COLOR_YELLOW:
+                color = R.color.playlistYellow;
+                break;
+            case Playlist.COLOR_GREEN:
+                color = R.color.playlistGreen;
+                break;
+            case Playlist.COLOR_BLUE:
+                color = R.color.playlistBlue;
+                break;
+        }
+        this.imgPlaylist.setBackgroundColor(this.getContext().getResources().getColor(color));
     }
 
     public interface OnPlaylistDialogListener {
